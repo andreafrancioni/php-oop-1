1c607ec33d82d804e2bc2f5e6e7b06daddd93e1a
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-13 13:49:45 +0100

    crea costruttore movie

--- Models/Movie.php
+++ Models/Movie.php
@@ -6,4 +6,12 @@ class Movie
     public $description;
     public $rating;
     public $publish_date;
+
+    public function __construct($title, $description, $rating, $publish_date)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->rating = $rating;
+        $this->publish_date = $publish_date;
+    }
 }
