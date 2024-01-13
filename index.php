f3e522fb92383ea036e1709bf48f4632a8d8b413
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-13 14:01:25 +0100

    Create due instanze movie e stampa a schermo con vardump

--- index.php
+++ index.php
@@ -8,7 +8,15 @@
 </head>
 
 <body>
+    <?php
+    require __DIR__ . '/Models/Movie.php';
 
+    $film1 = new Movie("Harry Potter e il calice di fuoco", "Harry e i suoi amici combattono per la vittoria", 5, 2006);
+    $film2 = new Movie("Avengers: Infinity War", "L'ultima battaglia è alle porte", 5, 2018);
+
+    var_dump($film1);
+    var_dump($film2);
+    ?>
 </body>
 
 </html>
